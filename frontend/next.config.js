cd837ce58405dece2df498d7e6b36bfc97867810
Bangtechuk <munimgujjar@icloud.com>
2025-04-08 21:17:03 +0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,5 +9,15 @@ const nextConfig = {
     domains: ['localhost', 'fittribe.fitness'],
   },
 }
+@type {import('next') .NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+    unoptimized: true,
+  },
+  output: 'export',
+}
 
+module.exports = nextConfig
 module.exports = nextConfig
